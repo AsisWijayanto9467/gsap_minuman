bddc7cbb51e786f010fcaffb0203c86745ce0521
AsisWijayanto9467 <sekolahasis@gmail.com>
2025-10-11 22:59:09 +0700

    Tambahan Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {useGSAP} from "@gsap/react";
+import { SplitText} from "gsap/all";
+import gsap from "gsap";
+
+const Hero = () => {
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', {type: 'chars, words'});
+        const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+        });
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1,
+        });
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+            .to('.right-leaf', {y: 200}, 0)
+            .to('.left-leaf', {y: -200}, 0)
+
+    }, [])
+  return (
+    <>
+        <section id="hero" className="noisy">
+            <h1 className="title">Wartman</h1>
+
+            <img
+                src="/images/hero-left-leaf.png"
+                alt="left-leaf"
+                className={"left-leaf"}
+            />
+
+            <img
+                src={"/images/hero-right-leaf.png"}
+                alt="right-leaf"
+                className={"right-leaf"}
+            />
+
+            <div className="body">
+                <div className="content">
+                    <div className={"space-x-5 hidden md:block"}>
+                        <p>Cool. Crisp. Classic</p>
+                        <p className={"subtitle"}>
+                            Satu Tegukan dari <br /> musim panas
+                        </p>
+                    </div>
+
+                    <div className={"view-cocktails"}>
+                        <p className={"subtitle"}>
+                            Setiap Minuman yang ada di menu kami, menawarkan sensai menyegarkan di panasnya bali,
+                            Setiap teguk, setiap es, setiap buah, setiap bahan dibuat sempurna untuk dirimu - dibuat untuk selera kamu.
+                        </p>
+                        <a href={"#cocktails"}>View Minumannya</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  );
+};
+
+export default Hero;
