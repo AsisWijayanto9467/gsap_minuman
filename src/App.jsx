e234fc47b6a9320f000e457d892af3eec5b736f2
AsisWijayanto9467 <sekolahasis@gmail.com>
2025-10-18 22:50:23 +0700

    Tmabah halaman Art

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Minuman from "./components/Minuman.jsx";
 import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -15,6 +16,7 @@ const App = () => {
         <Hero />
         <Minuman />
         <About />
+        <Art />
     </main>
   );
 };
